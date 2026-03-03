c82e6e793b84e9566397ae8fa49523081e6882a0
Manoel Victor Silveira <mano686@gmail.com>
2026-03-03 20:27:26 -0300

    commit

---
+++ aula15.py
@@ -0,0 +1,11 @@
+# name = input('Qual seu nome? ')
+# print(f'Olá, {name}! Bem-vindo(a) à aula 15 de Python!')
+
+
+numero_1 = input('Digite um número: ')
+numero_2 = input('Digite outro número: ')
+
+int_numero_1 = int(numero_1)
+int_numero_2 = int(numero_2)
+
+print(f'A soma dos números é: {int_numero_1 + int_numero_2}')
