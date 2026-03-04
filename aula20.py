02c5d5cd39ef4dc8da5dca10f89752efc0c15775
Manoel Victor Silveira <mano686@gmail.com>
2026-03-04 10:23:44 -0300

    commit

---
+++ aula20.py
@@ -0,0 +1,8 @@
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite um valor: ')
+
+if primeiro_valor > segundo_valor:
+    print(f'{primeiro_valor} é maior que {segundo_valor}') 
+elif primeiro_valor < segundo_valor:
+    print(f'{primeiro_valor} é menor que {segundo_valor}')
+else:    print(f'{primeiro_valor} é igual a {segundo_valor}')
