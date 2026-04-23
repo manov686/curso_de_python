3345c8ca425fedb29397a1c1dd6b4d805ebbe7f2
Manoel Victor Silveira <mano686@gmail.com>
2026-04-23 09:16:12 -0300

    commit

--- aula39.py
+++ aula39.py
@@ -2,4 +2,20 @@
 Iterando strings com while
 """
 
-nome = "Manoel" #Iterável
+nome = "Manoel"
+i = 0
+resultado = ""
+
+while True:
+    if i >= len(nome):
+        break
+
+    resultado += nome[i]
+
+    # Só adiciona o * se não for a última letra
+    if i < len(nome) - 1:
+        resultado += "*"
+
+    i += 1
+
+print(resultado)
