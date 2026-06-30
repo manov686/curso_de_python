48e697e8d12fe74221297365f6f476fd2d3563db
Manoel Victor Silveira <mano686@gmail.com>
2026-06-30 11:02:04 -0300

    commit

--- aula48.py
+++ aula48.py
@@ -7,13 +7,24 @@ Métodos úteis: append, insert, pop, del, clear, extend, +
 """
 #        +01234
 #        -54321
-string = 'ABCDE'  # 5 caracteres (len)
+#string = 'ABCDE'  # 5 caracteres (len)
 # print(bool([]))  # falsy
 # print(lista, type(lista))
 
 #        0    1      2              3    4
 #       -5   -4     -3             -2   -1
-lista = [123, True, 'Luiz Otávio',  1.2, []]
-lista[-3] = 'Manel'
-print(lista)
-print(lista[2], type(lista[2]))
+#lista = [123, True, 'Luiz Otávio',  1.2, []]
+#lista[-3] = 'Manel'
+#print(lista)
+#print(lista[2], type(lista[2]))
+
+
+lista = [10,20,30,40]
+#lista[2] = 300
+lista.append(50)
+lista.pop()
+lista.append(60)
+lista.append(70)
+ultimo_valor = lista.pop()
+#print(lista)
+print(f"{lista} removido: {ultimo_valor}")
---
+++ aula49.py
@@ -0,0 +1,1 @@
+#só dpois de finalizar lista
