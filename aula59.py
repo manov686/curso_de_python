7caaae318618fed958d922e40e3de444156f257a
Manoel Victor Silveira <mano686@gmail.com>
2026-07-16 13:06:10 -0300

    commit

---
+++ aula59.py
@@ -0,0 +1,25 @@
+# Desempacotamento em chamadas
+# de métodos e funções
+string = 'ABCD'
+lista = ['Maria', 'Helena', 1, 2, 3, 'Eduarda']
+tupla = 'Python', 'é', 'legal'
+salas = [
+    # 0        1
+    ['Maria', 'Helena', ],  # 0
+    # 0
+    ['Elaine', ],  # 1
+    # 0       1       2
+    ['Luiz', 'João', 'Eduarda', ],  # 2
+]
+
+# p, b, *_, ap, u = lista
+# print(p, u, ap)
+
+# print('Maria', 'Helena', 1, 2, 3, 'Eduarda')
+# print(*lista)
+# print(*string)
+# print(*tupla)
+# print(*salas, sep='\n')
+
+for i, sala in enumerate(salas, start=1):
+    print(f'Sala {i}:', *sala)
