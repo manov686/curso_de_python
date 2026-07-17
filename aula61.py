47f65338ed0883a2e9b36d59dd42d9eccc558296
Manoel Victor Silveira <mano686@gmail.com>
2026-07-17 09:13:12 -0300

    commit

--- aula61.py
+++ aula61.py
@@ -30,39 +30,47 @@ O primeiro dígito do CPF é 7
 #     .replace('/', '')\
 #     .replace(' ', '')
 
-import re
+# import re
 
-cpf_enviado = re.sub(r'[^0-9]', '', input('Digite o CPF: '))
-entrada_repetida = cpf_enviado == cpf_enviado[0] * len(cpf_enviado)
+# cpf_enviado = re.sub(r'[^0-9]', '', input('Digite o CPF: '))
+# entrada_repetida = cpf_enviado == cpf_enviado[0] * len(cpf_enviado)
 
-if len(cpf_enviado) != 11 or entrada_repetida:
-    print('CPF inválido!')
-    exit()
+# if len(cpf_enviado) != 11 or entrada_repetida:
+#     print('CPF inválido!')
+#     exit()
 
-nove_digitos = str(cpf_enviado)[:9]
-contador_regressivo_1 = 10
+import random
 
-resultado_dig_1 = 0
-for digito_1 in nove_digitos:
-    resultado_dig_1 += int(digito_1) * contador_regressivo_1
-    contador_regressivo_1 -= 1
+for _ in range(100):
+    nove_digitos = ''
+    for i in range(9):
+        nove_digitos += str(random.randint(0, 9))
 
-digito_1 = (resultado_dig_1*10 % 11 if resultado_dig_1 * 10 % 11 <= 9 else 0)
+    contador_regressivo_1 = 10
+
+    resultado_dig_1 = 0
+    for digito_1 in nove_digitos:
+        resultado_dig_1 += int(digito_1) * contador_regressivo_1
+        contador_regressivo_1 -= 1
+
+    digito_1 = (resultado_dig_1*10 % 11 if resultado_dig_1 * 10 % 11 <= 9 else 0)
 
 
-dez_digitos = str(cpf_enviado)[:10]
-contador_regressivo_2 = 11
+    dez_digitos = nove_digitos + str(digito_1)
+    contador_regressivo_2 = 11
 
-resultado_dig_2 = 0
-for digito_2 in dez_digitos:
-    resultado_dig_2 += int(digito_2) * contador_regressivo_2
-    contador_regressivo_2 -= 1
+    resultado_dig_2 = 0
+    for digito_2 in dez_digitos:
+        resultado_dig_2 += int(digito_2) * contador_regressivo_2
+        contador_regressivo_2 -= 1
 
-digito_2 = (resultado_dig_2*10 % 11 if resultado_dig_2 * 10 % 11 <= 9 else 0)
+    digito_2 = (resultado_dig_2*10 % 11 if resultado_dig_2 * 10 % 11 <= 9 else 0)
 
-cpf_calculado = f'{nove_digitos}{digito_1}{digito_2}'
+    cpf_calculado = f'{nove_digitos}{digito_1}{digito_2}'
 
-if cpf_enviado == cpf_calculado:
-    print(f'{cpf_calculado}: CPF Válido')
-else:
-    print(f'{cpf_calculado}: CPF Inválido')
+    print(cpf_calculado)
+
+# if cpf_enviado == cpf_calculado:
+#     print(f'{cpf_calculado}: CPF Válido')
+# else:
+#     print(f'{cpf_calculado}: CPF Inválido')
