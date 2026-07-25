159ac42f64e26f62de7e3c10c587346dc901981b
Manoel Victor Silveira <mano686@gmail.com>
2026-07-25 16:57:44 -0300

    commit

--- aula76.py
+++ aula76.py
@@ -1,49 +1,29 @@
-# Dicionários em Python (tipo dict)
-# Dicionários são estruturas de dados do tipo
-# par de "chave" e "valor".
-# Chaves podem ser consideradas como o "índice"
-# que vimos na lista e podem ser de tipos imutáveis
-# como: str, int, float, bool, tuple, etc.
-# O valor pode ser de qualquer tipo, incluindo outro
-# dicionário.
-# Usamos as chaves - {} - ou a classe dict para criar
-# dicionários.
-# Imutáveis: str, int, float, bool, tuple
-# # Mutável: dict, list
+# Métodos úteis dos dicionários em Python
+# len - quantas chaves
+# keys - iterável com as chaves
+# values - iterável com os valores
+# items - iterável com chaves e valores
+# setdefault - adiciona valor se a chave não existe
+# copy - retorna uma cópia rasa (shallow copy)
+# get - obtém uma chave
+# pop - Apaga um item com a chave especificada (del)
+# popitem - Apaga o último item adicionado
+# update - Atualiza um dicionário com outro
+pessoa = {
+    'nome:': 'Luiz Otávio',
+    'sobrenome:': 'Miranda',
+    # 'idade': 900,
+}
 
-# person = {
-#     'Nome:': 'Manoel Victor',
-#     'Sobrenome:': 'Silveira',
-#     'Idade:': 35,
-#     'Altura:': 1.88,
-#     'Endereços:': [
-#         {'rua': 'ten. da cunha silva', 'número': 123},
-#         {'rua': 'olimpia cruz', 'número': 321},
-#     ],
-# }
+pessoa.setdefault('idade', 0)
+# print(pessoa['idade'])
+# print(len(pessoa))
+# print(list(pessoa.keys()))
+# print(list(pessoa.values()))
+# print(list(pessoa.items()))
 
-# for key in person:
-#     print(key, person[key])
-pessoa = {}
+for chave, valor in pessoa.items():
+    print(chave, valor)
 
-chave = 'nome'
-
-pessoa[chave] = 'Manoel Victor'
-pessoa['sobrenome'] = 'Silveira'
-
-
-print(pessoa[chave])
-
-pessoa[chave] = 'Maria'
-
-del pessoa['sobrenome']
-print(pessoa)
-print(pessoa['nome'])
-
-# print(pessoa.get('sobrenome'))
-if pessoa.get('sobrenome') is None:
-    print('NÃO EXISTE')
-else:
-    print(pessoa['sobrenome'])
-
-# print('ISSO Não vai')
+# for chave, valor in pessoa.items():
+#     print(chave, valor)
