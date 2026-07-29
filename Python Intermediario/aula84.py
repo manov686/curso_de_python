c23863cdad8ac6e086b8d3dbf6a8272692b47fd8
Manoel Victor Silveira <mano686@gmail.com>
2026-07-29 17:00:33 -0300

    Refactor aula84.py: Clean up commented-out code and enhance product price adjustment logic

--- Python Intermediario/aula84.py
+++ Python Intermediario/aula84.py
@@ -7,8 +7,33 @@
 #     lista.append(numero)
 # print(lista)
 
-lista = [
-    numero * 2
-    for numero in range(10)
+# lista = [
+#     numero * 2
+#     for numero in range(10)
+# ]
+# print(lista)
+
+# import pprint
+
+
+# def p(v):
+#     pprint.pprint(v, sort_dicts=False, width=40)
+
+produtos = [
+    {'nome': 'frango', 'preco': 20, 'validade': 2026},
+    {'nome': 'acem', 'preco': 30, 'validade': 2026},
+    {'nome': 'coxao mole', 'preco': 40, 'validade': 2026}
 ]
-print(lista)
+
+reajuste = [
+    {**produto, 'preco': produto['preco'] * 1.05}
+    for produto in produtos
+]
+
+print(
+    *[
+        f"{produto['nome']}: R$ {produto['preco']:.2f}".replace('.', ',')
+        for produto in reajuste
+    ],
+    sep='\n'
+)
