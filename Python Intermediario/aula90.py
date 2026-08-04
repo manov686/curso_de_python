87177e9dc58d149fcf8ffebbbc5a495ae0d6246a
Manoel Victor Silveira <mano686@gmail.com>
2026-08-04 19:08:49 -0300

    Refactor aula90.py: Uncomment code examples for generator expressions and improve readability

--- Python Intermediario/aula90.py
+++ Python Intermediario/aula90.py
@@ -1,18 +1,20 @@
-# import sys
+import sys
 
 # Generator expression, Iterables e Iterators em Python
 iterable = ['Eu', 'Tenho', '__iter__']
 iterator = iter(iterable)  # tem __iter__ e __next__
-print(next(iterator))
-print(next(iterator))
-print(next(iterator))   
-# lista = [n for n in range(1000000)]
-# generator = (n for n in range(1000000))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))   
+lista = [n for n in range(10000)]
+generator = (n for n in range(10000))
 
-# print(sys.getsizeof(lista))
-# print(sys.getsizeof(generator))
+print(sys.getsizeof(lista))
+print(sys.getsizeof(generator))
 
-# print(generator)
+# print(next(generator))
+# print(next(generator))
+# print(next(generator))
 
-# # for n in generator:
-# #     print(n)
+for n in generator:
+    print(n)
