b721c5ba6eeab42c84341bf2b44f4225bd47e3b5
Manoel Victor Silveira <mano686@gmail.com>
2026-08-06 11:14:37 -0300

    Refactor aula93.py: Move variable assignments inside try block for better error handling

--- Python Intermediario/aula93.py
+++ Python Intermediario/aula93.py
@@ -1,14 +1,18 @@
 # Try, except, else e finally
 
-a = 18
-b = 0
-c = a/b
+# a = 18
+# b = 0
+# c = a/b
 
 try:
+    a = 18
+    b = 0
     c = a/b
 except NameError:
     print('Variável não definida!')
 except ZeroDivisionError:
     print('Não é possível dividir por zero!')
+except Exception as erro:
+    print(f'Ocorreu um erro: {erro.__class__}')
 
 print('O código continua...')
