00e0e1aa3c0a9d54dd572f0a742e2e546b339558
Manoel Victor Silveira <mano686@gmail.com>
2026-08-09 08:38:05 -0300

    Refactor aula95.py: Implement error handling for division and type validation

--- Python Intermediario/aula95.py
+++ Python Intermediario/aula95.py
@@ -1,5 +1,30 @@
 #raise - lançando excessões
 
-print(123)
-raise ValueError('Error')
-print(456)
+# print(123)
+# raise ValueError('Error')
+# print(456)
+
+def nao_aceito_zero(d):
+    if d == 0:
+        raise ZeroDivisionError('Você está tentando dividir por zero')
+    return True
+
+
+def deve_ser_int_ou_float(n):
+    tipo_n = type(n)
+    if not isinstance(n, (float, int)):
+        raise TypeError(
+            f'"{n}" deve ser int ou float. '
+            f'"{tipo_n.__name__}" enviado.'
+        )
+    return True
+
+
+def divide(n, d):
+    deve_ser_int_ou_float(n)
+    deve_ser_int_ou_float(d)
+    nao_aceito_zero(d)
+    return n / d
+
+
+print(divide(8, '0'))
