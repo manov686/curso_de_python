d780a51cdfb2a131cd1872e5f64254d1f93508e4
Manoel Victor Silveira <mano686@gmail.com>
2026-08-11 16:16:30 -0300

    Add fala_oi function and update aula99.py: Introduce new module for greeting and refactor imports

--- Python Intermediario/aula99.py
+++ Python Intermediario/aula99.py
@@ -1,8 +1,18 @@
-from sys import path
+# from sys import path
 
-from aula99_package.modulo import soma_do_modulo
+# import aula99_package.modulo
+# from aula99_package import modulo
+# from aula99_package.modulo import *
 
-print(soma_do_modulo(10, 20))
+# # from aula99_package.modulo import soma_do_modulo
 
+# # print(*path, sep='\n')
+# print(soma_do_modulo(1, 2))
+# print(aula99_package.modulo.soma_do_modulo(1, 2))
+# print(modulo.soma_do_modulo(1, 2))
+# print(variavel)
+# print(nova_variavel)
+from aula99_package.modulo import fala_oi, soma_do_modulo
 
-# print(*path, sep='\n')
+print(__name__)
+fala_oi()
