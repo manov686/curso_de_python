3c623f0bfaa933602222a6f13f54cbe0bcf5f0d1
Manoel Victor Silveira <mano686@gmail.com>
2026-08-13 11:33:06 -0300

    Add produtos_modulo.py: Define product list with pricing for exercises Update aula100.py: Import product list and adjust sorting output format

--- Python Intermediario/aula100.py
+++ Python Intermediario/aula100.py
@@ -2,13 +2,6 @@
 # Exercícios
 # Aumente os preços dos produtos a seguir em 10%
 # Gere novos_produtos por deep copy (cópia profunda)
-produtos = [
-    {'nome': 'Produto 5', 'preco': 10.00},
-    {'nome': 'Produto 1', 'preco': 22.32},
-    {'nome': 'Produto 3', 'preco': 10.11},
-    {'nome': 'Produto 2', 'preco': 105.87},
-    {'nome': 'Produto 4', 'preco': 69.90},
-]
 
 # Ordene os produtos por nome decrescente (do maior para menor)
 # Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
@@ -16,7 +9,10 @@ produtos = [
 # Ordene os produtos por preco crescente (do menor para maior)
 # Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
 
+
 import copy
+
+from dados import produtos
 
 novos_produtos = copy.deepcopy(produtos)
 
@@ -36,6 +32,6 @@ produtos_ordenados_por_preco = sorted(
     key=lambda produto: produto['preco']
 )
 
-print(produtos_ordenados_por_nome)
+print(*produtos_ordenados_por_nome, sep='\n')
 print()
-print(produtos_ordenados_por_preco)
+print(*produtos_ordenados_por_preco, sep='\n')
