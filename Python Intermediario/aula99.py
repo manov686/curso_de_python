a9555ac89ad63f9f81acbdce8fabe550dab1be46
Manoel Victor Silveira <mano686@gmail.com>
2026-08-13 10:23:49 -0300

    Refactor aula99.py and update aula99_package: Adjust imports and add example usage for soma_do_modulo

--- Python Intermediario/aula99.py
+++ Python Intermediario/aula99.py
@@ -12,7 +12,9 @@
 # print(modulo.soma_do_modulo(1, 2))
 # print(variavel)
 # print(nova_variavel)
-from aula99_package.modulo import fala_oi, soma_do_modulo
+# from aula99_package.modulo import fala_oi, soma_do_modulo
 
-print(__name__)
-fala_oi()
+# import aula99_package
+from aula99_package import soma_do_modulo
+
+print(soma_do_modulo(2, 3))
---
+++ Python Intermediario/aula99_package/__init__.py
@@ -0,0 +1,8 @@
+# print('' \
+# 'você importou o package', __name__
+# )
+
+# def dobra(x):
+#     return x * 2
+
+from .modulo import soma_do_modulo, variavel, nova_variavel
