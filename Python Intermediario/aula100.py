94e6b5f1cffd24552848cc9d934a68b68f7707f5
Manoel Victor Silveira <mano686@gmail.com>
2026-08-13 18:28:08 -0300

    Fix product sorting and ensure price increase is applied correctly

--- Python Intermediario/aula100.py
+++ Python Intermediario/aula100.py
@@ -9,11 +9,11 @@
 # Ordene os produtos por preco crescente (do menor para maior)
 # Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
 
-
 import copy
 
 from dados import produtos
 
+# Aumentar os preços em 10%
 novos_produtos = copy.deepcopy(produtos)
 
 for produto in novos_produtos:
@@ -21,17 +21,26 @@ for produto in novos_produtos:
 
 # Ordenar por nome decrescente
 produtos_ordenados_por_nome = sorted(
-    copy.deepcopy(novos_produtos),
+    copy.deepcopy(produtos),
     key=lambda produto: produto['nome'],
-    reverse=True
+    reverse=False
 )
 
 # Ordenar por preço crescente
 produtos_ordenados_por_preco = sorted(
-    copy.deepcopy(novos_produtos),
+    copy.deepcopy(produtos),
     key=lambda produto: produto['preco']
 )
 
+print(*novos_produtos, sep='\n')
+print()
 print(*produtos_ordenados_por_nome, sep='\n')
 print()
 print(*produtos_ordenados_por_preco, sep='\n')
+
+# for original, novo in zip(produtos, novos_produtos):
+#     esperado = round(original['preco'] * 1.1, 2)
+
+#     assert novo['preco'] == esperado
+
+# print('Todos os preços foram calculados corretamente!')
