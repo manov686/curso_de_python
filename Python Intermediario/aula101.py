c3efb07a4197c3ca6eec40cfefda67460d05325d
Manoel Victor Silveira <mano686@gmail.com>
2026-08-14 18:53:30 -0300

    Refactor criar_funcao to accept a fixed argument and return a closure for deferred execution

--- Python Intermediario/aula101.py
+++ Python Intermediario/aula101.py
@@ -7,9 +7,15 @@ def multiplica(x, y):
     return x * y
 
 
-def criar_funcao(funcao, *args):
-    return funcao(*args)
+def criar_funcao(funcao, x):
+    def funcao_adicionada(*args):
+        return funcao(x, *args)
+    return funcao_adicionada
 
 
 soma_com_cinco = criar_funcao(soma, 5)
 multiplica_por_dez = criar_funcao(multiplica, 10)
+
+
+print(soma_com_cinco(3))  # Saída: 8
+print(multiplica_por_dez(4))  # Saída: 40
