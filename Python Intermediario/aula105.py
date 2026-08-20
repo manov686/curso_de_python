f055261338cbcc98718a189a7f7bbf13c433bd3f
Manoel Victor Silveira <mano686@gmail.com>
2026-08-20 12:36:17 -0300

    Refactor aula105.py: Simplify decorator factory implementation and improve parameter logging

--- Python Intermediario/aula105.py
+++ Python Intermediario/aula105.py
@@ -1,26 +1,25 @@
-# Decoradores com parâmetros
-def fabrica_de_decoradores(a=None, b=None, c=None):
-    def fabrica_de_funcoes(func):
-        print('Decoradora 1')
+def fabrica_decoradores(a=None, b=None, c=None):
+
+    def decoradora(func):
 
         def aninhada(*args, **kwargs):
-            print('Parâmetros do decorador, ', a, b, c)
-            print('Aninhada')
-            res = func(*args, **kwargs)
-            return res
+            print(f'Decorador: {a=}, {b=}, {c=}')
+            return func(*args, **kwargs)
+
         return aninhada
-    return fabrica_de_funcoes
+
+    return decoradora
 
 
-@fabrica_de_decoradores(1, 2, 3)
+@fabrica_decoradores(1, 2, 3)
 def soma(x, y):
     return x + y
 
 
-decoradora = fabrica_de_decoradores()
-multiplica = decoradora(lambda x, y: x * y)
+@fabrica_decoradores()
+def multiplica(x, y):
+    return x * y
 
-dez_mais_cinco = soma(10, 5)
-dez_vezes_cinco = multiplica(10, 5)
-print(dez_mais_cinco)
-print(dez_vezes_cinco)
+
+print(soma(10, 5))
+print(multiplica(10, 5))
