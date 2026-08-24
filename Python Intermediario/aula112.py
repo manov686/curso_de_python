82758e2bdfe6f9dddabe4fa1fc25a7cee4ea6bb9
Manoel Victor Silveira <mano686@gmail.com>
2026-08-24 15:37:08 -0300

    Refactor aula112.py: Uncomment and clean up filter example, implement total calculation using reduce

--- Python Intermediario/aula112.py
+++ Python Intermediario/aula112.py
@@ -1,4 +1,8 @@
 # filter é um filtro funcional
+# reduce
+
+from functools import reduce
+
 def print_iter(iterator):
     print(*list(iterator), sep='\n')
     print()
@@ -20,13 +24,27 @@ def filtrar_preco(produto):
 # novos_produtos = [
 #     p for p in produtos
 #     if p['preco'] > 100
-# ]
-novos_produtos = filter(
-    # lambda produto: produto['preco'] > 100,
-    filtrar_preco,
-    produtos
-)
+# # ]
+# novos_produtos = filter(
+#     # lambda produto: produto['preco'] > 100,
+#     filtrar_preco,
+#     produtos
+# )
 
+# total = 0
+# for produto in produtos:
+#     total += produto['preco']
 
 print_iter(produtos)
-print_iter(novos_produtos)
+# print_iter(novos_produtos)
+
+# print(sum([produto['preco'] for produto in produtos]))
+
+total = reduce(
+    lambda acumulador, produto: acumulador + produto['preco'],
+    produtos,
+    0
+)
+
+print(f'Total: {total:.2f} R$')
+
