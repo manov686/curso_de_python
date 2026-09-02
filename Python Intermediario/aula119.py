02ac71f670267c024f90471fc75e9fd87c1925c1
Manoel Victor Silveira <mano686@gmail.com>
2026-09-02 11:45:42 -0300

    Refactor task list implementation: Enhance add_task function and improve command handling

--- Python Intermediario/aula119.py
+++ Python Intermediario/aula119.py
@@ -9,4 +9,78 @@
 # refazer = todo ['fazer café']
 # refazer = todo ['fazer café', 'caminhar']
 
-lista = input()
+import os
+
+import subprocess
+
+def add_task(tarefa, lista=None):
+
+    if lista is None:
+
+        lista = []
+
+    lista.append(tarefa)
+
+    return lista
+
+todo = []
+
+desfazer = []
+
+while True:
+
+    entrada = input('Digite uma tarefa ou comando (listar, desfazer, refazer, limpar, sair): ')
+
+    if entrada == 'listar':
+
+        print('\nLista de tarefas:')
+
+        for tarefa in todo:
+
+            print(f'- {tarefa}')
+
+    elif entrada == 'desfazer':
+
+        if todo:
+
+            tarefa = todo.pop()
+
+            desfazer.append(tarefa)
+
+            print(f'Tarefa desfeita: {tarefa}')
+
+        else:
+
+            print('Não há tarefas para desfazer.')
+
+    elif entrada == 'refazer':
+
+        if desfazer:
+
+            tarefa = desfazer.pop()
+
+            todo.append(tarefa)
+
+            print(f'Tarefa refeita: {tarefa}')
+
+        else:
+
+            print('Não há tarefas para refazer.')
+
+    elif entrada == 'limpar':
+
+        subprocess.run(['cls'] if os.name == 'nt' else ['clear'], shell=True)
+
+    elif entrada == 'sair':
+
+        print('Programa encerrado.')
+
+        break
+
+    else:
+
+        print('Comando inválido.')
+
+        add_task(entrada, todo)
+
+        desfazer.clear()
