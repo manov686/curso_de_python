a2439b1e05181780a6972474bad215275e789e22
Manoel Victor Silveira <mano686@gmail.com>
2026-09-02 10:25:15 -0300

    Update aula117.py and add aula117.json: Modify person object details and create JSON file for serialization

--- Python Intermediario/aula117.py
+++ Python Intermediario/aula117.py
@@ -1,13 +1,13 @@
 import json
 
 pessoa = {
-    'nome': 'Luiz Otávio 2',
-    'sobrenome': 'Miranda',
+    'nome': 'Manoel Victor',
+    'sobrenome': 'Silveira',
     'enderecos': [
-        {'rua': 'R1', 'numero': 32},
-        {'rua': 'R2', 'numero': 55},
+        {'rua': 'Rua João Cruz Silva', 'numero': 32},
+        {'rua': 'Rua Maria Madalena', 'numero': 55},
     ],
-    'altura': 1.8,
+    'altura': 1.88,
     'numeros_preferidos': (2, 4, 6, 8, 10),
     'dev': True,
     'nada': None,
