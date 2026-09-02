59416a1a0907dca8fc453f7ff2f354c6aff322f3
Manoel Victor Silveira <mano686@gmail.com>
2026-09-02 11:55:02 -0300

    Refactor task handling: Rename todo list to tarefas and improve code readability

--- Python Intermediario/aula119.py
+++ Python Intermediario/aula119.py
@@ -10,77 +10,67 @@
 # refazer = todo ['fazer café', 'caminhar']
 
 import os
-
 import subprocess
 
+
 def add_task(tarefa, lista=None):
-
     if lista is None:
-
         lista = []
 
     lista.append(tarefa)
 
     return lista
 
-todo = []
 
+tarefas = []
 desfazer = []
+
 
 while True:
 
-    entrada = input('Digite uma tarefa ou comando (listar, desfazer, refazer, limpar, sair): ')
+    entrada = input(
+        'Digite uma tarefa ou comando '
+        '(listar, desfazer, refazer, limpar, sair): '
+    )
 
     if entrada == 'listar':
 
         print('\nLista de tarefas:')
 
-        for tarefa in todo:
-
+        for tarefa in tarefas:
             print(f'- {tarefa}')
 
     elif entrada == 'desfazer':
 
-        if todo:
-
-            tarefa = todo.pop()
-
+        if tarefas:
+            tarefa = tarefas.pop()
             desfazer.append(tarefa)
-
             print(f'Tarefa desfeita: {tarefa}')
-
         else:
-
             print('Não há tarefas para desfazer.')
 
     elif entrada == 'refazer':
 
         if desfazer:
-
             tarefa = desfazer.pop()
-
-            todo.append(tarefa)
-
+            tarefas.append(tarefa)
             print(f'Tarefa refeita: {tarefa}')
-
         else:
-
             print('Não há tarefas para refazer.')
 
     elif entrada == 'limpar':
 
-        subprocess.run(['cls'] if os.name == 'nt' else ['clear'], shell=True)
+        subprocess.run(
+            ['cls'] if os.name == 'nt' else ['clear'],
+            shell=True
+        )
 
     elif entrada == 'sair':
 
         print('Programa encerrado.')
-
         break
 
     else:
 
-        print('Comando inválido.')
-
-        add_task(entrada, todo)
-
+        add_task(entrada, tarefas)
         desfazer.clear()
