9e0f2754272ada17b2d131f96cc15390b2512df4
Manoel Victor Silveira <mano686@gmail.com>
2026-09-09 13:51:59 -0300

    Format JSON task file for better readability and update task loading/saving functions to maintain formatting

--- Python Intermediario/aula119-2.py
+++ Python Intermediario/aula119-2.py
@@ -56,10 +56,10 @@ def ler(tarefas, caminho_arquivo):
     with open(caminho_arquivo, 'r', encoding='utf-8') as arquivo:
         tarefas_json = arquivo.read()
         tarefas.extend(json.loads(tarefas_json))
-        
+
 def salvar(tarefas, caminho_arquivo):
     with open(caminho_arquivo, 'w', encoding='utf-8') as arquivo:
-        tarefas_json = json.dumps(tarefas)
+        tarefas_json = json.dumps(tarefas, indent = 2, ensure_ascii=False)
         arquivo.write(tarefas_json)
 
 def sair(tarefas, caminho_arquivo):
