7a11e69e004ede78bde67e0b105611e134f7b399
Manoel Victor Silveira <mano686@gmail.com>
2026-09-10 18:17:16 -0300

    Fix Car class initialization and update methods for consistency

--- Python Intermediario/aula121.py
+++ Python Intermediario/aula121.py
@@ -1,9 +1,27 @@
 # Métodos em instâncias de classes Python
 
 class Car:
-    def __init__(self, name, model):
-        self.name = name
+    def __init__(self, brand, model):
+        self.brand = brand
         self.model = model
 
     def description(self):
-        return f'{self.name} {self.model}'
+        return f'{self.brand} {self.model}'
+    def acelerate(self):
+        return f'{self.model} is acelerating!'
+
+car1 = Car("Toyota", "Corolla")
+car2 = Car("Honda", "Civic")
+car3 = Car("Volkwagen", "Beetle")
+
+# print(car1.description())
+# print(car2.description())
+# print(car3.description())
+# print()
+# print(f'Carro 1:  {car1.model}')
+# print(f'Carro 2:  {car2.model}')
+# print(f'Carro 3:  {car3.model}')
+
+print(car1.acelerate())
+print(car2.acelerate())
+print(car3.acelerate())
