7f0821e57dea98e6133773ee026b2380e9900ead
Manoel Victor Silveira <mano686@gmail.com>
2026-09-12 11:13:10 -0300

    Refactor pessoa class to remove unused attribute and ensure consistent year calculation

--- Python Intermediario/aula125.py
+++ Python Intermediario/aula125.py
@@ -3,8 +3,16 @@
 ANO_ATUAL = 2026
 
 class pessoa:
-    atributo = 'valor'
+    ano_atual = 2026
 
     def __init__(self, nome, idade):
         self.nome = nome
         self.idade = idade
+
+    def get_ano_nascimento(self):
+        return pessoa.ano_atual - self.idade
+
+p1 = pessoa('Manoel', 35)
+p2 = pessoa('Maria', 20)
+print(p1.get_ano_nascimento())
+print(p2.get_ano_nascimento())
