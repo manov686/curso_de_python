357052486b5fa32a8c7c0201927e52dacdb2bd9b
Manoel Victor Silveira <mano686@gmail.com>
2026-09-12 08:02:35 -0300

    Refactor Animal class to include 'executar' method for improved method access

--- Python Intermediario/aula123.py
+++ Python Intermediario/aula123.py
@@ -6,9 +6,12 @@ class Animal:
         self.nome = nome  # atributo de instância
     def acao(self, alimento):
         return f'O {self.nome} está comendo {alimento}'
+    def executar(self, *args, **kwargs):
+        return self.acao(*args, **kwargs)
 
 animal1 = Animal('tigre')
 
 # print(Animal.nome)          # acessando atributo de classe
 print(animal1.nome)           # acessando atributo de instância
-print(animal1.acao('carne'))  # acessando método da instância
+# print(animal1.acao('carne'))  # acessando método da instância
+print(animal1.executar('carne'))  # acessando método da instância através do método executar
