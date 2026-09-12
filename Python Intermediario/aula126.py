e196a5f5be637832b68d7e8a9eb5da94967e63c4
Manoel Victor Silveira <mano686@gmail.com>
2026-09-12 11:17:17 -0300

    Refactor Pessoa class to include constructor and method for calculating birth year

--- Python Intermediario/aula126.py
+++ Python Intermediario/aula126.py
@@ -1,3 +1,13 @@
 # __dict__ e vars para atributos de instância
 class Pessoa:
     ano_atual = 2026
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def get_ano_nascimento(self):
+        return self.ano_atual - self.idade
+
+p1 = Pessoa('Manoel', 35)
+print(p1.__dict__)  # Mostra os atributos de instância do objeto p1
