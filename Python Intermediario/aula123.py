82ed1d7daf0c36b1185e92b4b1c7ed01ffd82878
Manoel Victor Silveira <mano686@gmail.com>
2026-09-12 07:59:47 -0300

    Add implementation of Animal class with methods for instance attributes and actions

--- Python Intermediario/aula123.py
+++ Python Intermediario/aula123.py
@@ -1,2 +1,14 @@
 # Escopo da classe e de métodos da classe
 class Animal:
+    # nome = 'leão'  # atributo de classe
+
+    def __init__(self, nome):
+        self.nome = nome  # atributo de instância
+    def acao(self, alimento):
+        return f'O {self.nome} está comendo {alimento}'
+
+animal1 = Animal('tigre')
+
+# print(Animal.nome)  # acessando atributo de classe
+print(animal1.nome)  # acessando atributo de instância
+print(animal1.acao('carne'))  # acessando método da instância
