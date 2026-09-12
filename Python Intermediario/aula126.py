0d2e741c7490433d3ccf26b9364bfdb4465b76ab
Manoel Victor Silveira <mano686@gmail.com>
2026-09-12 11:21:20 -0300

    Refactor instantiation of Pessoa class to use dictionary unpacking

--- Python Intermediario/aula126.py
+++ Python Intermediario/aula126.py
@@ -9,5 +9,7 @@ class Pessoa:
     def get_ano_nascimento(self):
         return self.ano_atual - self.idade
 
-p1 = Pessoa('Manoel', 35)
+dados_p1 = {'nome': 'Manoel', 'idade': 35}
+p1 = Pessoa(**dados_p1)  # Desempacotando o dicionário para criar o objeto p1
+# p1 = Pessoa('Manoel', 35)
 print(p1.__dict__)  # Mostra os atributos de instância do objeto p1
