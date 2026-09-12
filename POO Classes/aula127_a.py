8258e0633e8ee411361aba0ac45a399275e603cd
Manoel Victor Silveira <mano686@gmail.com>
2026-09-12 11:50:18 -0300

    Refactor JSON handling: remove old aula127.json and update aula127_a.py to save new instances of Pessoa class

--- POO Classes/aula127_a.py
+++ POO Classes/aula127_a.py
@@ -4,6 +4,9 @@
 # da classe com os dados salvos
 # Faça em arquivos separados.
 # from datetime import datetime
+import json
+
+CAMINHO_ARQUIVO = 'aula127.json'
 
 class Pessoa:
     def __init__(self, nome, idade):
@@ -12,3 +15,20 @@ class Pessoa:
 
     def __repr__(self):
         return f'Pessoa(nome={self.nome!r}, idade={self.idade})'
+
+p1 = Pessoa('Manoel', 35)
+p2 = Pessoa('Maria', 28)
+p3 = Pessoa('José', 42)
+
+pessoas = [vars(p1), vars(p2), vars(p3)]
+
+dados = [
+    {
+        'nome': pessoa['nome'],
+        'idade': pessoa['idade']
+    }
+    for pessoa in pessoas
+]
+
+with open(CAMINHO_ARQUIVO, 'w', encoding='utf-8') as arquivo:
+    json.dump(dados, arquivo, ensure_ascii=False, indent=2)
--- POO Classes/aula127_b.py
+++ POO Classes/aula127_b.py
@@ -1,24 +1,6 @@
-CAMINHO_ARQUIVO = 'aula127.json'
-
 import json
-from aula127_a import Pessoa
+from aula127_a import CAMINHO_ARQUIVO, Pessoa
 
-p1 = Pessoa('Manoel', 35)
-p2 = Pessoa('Maria', 28)
-p3 = Pessoa('José', 42)
-
-pessoas = [vars(p1), vars(p2), vars(p3)]
-
-dados = [
-    {
-        'nome': pessoa['nome'],
-        'idade': pessoa['idade']
-    }
-    for pessoa in pessoas
-]
-
-with open(CAMINHO_ARQUIVO, 'w', encoding='utf-8') as arquivo:
-    json.dump(dados, arquivo, ensure_ascii=False, indent=2)
 
 with open(CAMINHO_ARQUIVO, 'r', encoding='utf-8') as arquivo:
     dados = json.load(arquivo)
