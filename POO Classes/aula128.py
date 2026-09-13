60b8fa57160438fcd6bf2d49ded25d9a25b35f31
Manoel Victor Silveira <mano686@gmail.com>
2026-09-13 14:09:56 -0300

    Add aula128.py: Implement class methods and factory pattern for Pessoa class

---
+++ POO Classes/aula128.py
@@ -0,0 +1,10 @@
+# Métodos de classe + factories (fábricas)
+# São métodos onde "self" será "cls", ou seja,
+# ao invés de receber a instância no primeiro
+# parâmetro, receberemos a própria classe.
+class Pessoa:
+    ano = 2023  # atributo de classe
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
