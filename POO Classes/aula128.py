67c4597262dc12390be4d3b4c93f7ae54a74a33e
Manoel Victor Silveira <mano686@gmail.com>
2026-09-13 14:26:47 -0300

    Rename class from 'Pessoa' to 'Person' and add class method for instance creation

--- POO Classes/aula128.py
+++ POO Classes/aula128.py
@@ -2,9 +2,17 @@
 # São métodos onde "self" será "cls", ou seja,
 # ao invés de receber a instância no primeiro
 # parâmetro, receberemos a própria classe.
-class Pessoa:
+class Person:
     year = 2026  # atributo de classe
 
     def __init__(self, name, age):
         self.nome = name
         self.idade = age
+
+    @classmethod
+
+    def metodo_de_classe(cls, name, age):
+        return cls(name, age)
+
+p1 = Person('Luiz', 30)
+# print(p1.metodo_de_classe())
