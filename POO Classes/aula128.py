33e7f864852cf0d8dbcb14b7bd560f4d4c5b6321
Manoel Victor Silveira <mano686@gmail.com>
2026-09-13 14:10:23 -0300

    Update aula128.py: Change class attribute name from 'ano' to 'year' and update constructor parameter names to 'name' and 'age'

--- POO Classes/aula128.py
+++ POO Classes/aula128.py
@@ -3,8 +3,8 @@
 # ao invés de receber a instância no primeiro
 # parâmetro, receberemos a própria classe.
 class Pessoa:
-    ano = 2023  # atributo de classe
+    year = 2026  # atributo de classe
 
-    def __init__(self, nome, idade):
-        self.nome = nome
-        self.idade = idade
+    def __init__(self, name, age):
+        self.nome = name
+        self.idade = age
