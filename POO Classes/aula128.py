4d40f1d84e8a09f0eae0c09e7d2cde71a09f30d9
Manoel Victor Silveira <mano686@gmail.com>
2026-09-13 14:35:06 -0300

    Add class method to Person class for unnamed instance creation

--- POO Classes/aula128.py
+++ POO Classes/aula128.py
@@ -12,8 +12,13 @@ class Person:
     @classmethod
     def metodo_de_classe(cls, name, age):
         return cls(name, age)
+    @classmethod
+    def sem_nome(cls, age):
+        return cls('Anônimo', age)
 
 p1 = Person.metodo_de_classe('Luiz', 30)
 p2 = Person.metodo_de_classe('Maria', 20)
+p3 = Person.sem_nome(25)
 print(p1.nome, p1.idade)
 print(p2.nome, p2.idade)
+print(p3.nome, p3.idade)
