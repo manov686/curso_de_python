921dfd7627f32ea73f71102af0e91235f238a130
Manoel Victor Silveira <mano686@gmail.com>
2026-09-14 10:19:04 -0300

    Add Connection class with constructor for database connection

---
+++ POO Classes/aula130.py
@@ -0,0 +1,6 @@
+# method vs @classmethod vs @staticmethod
+# method - self, método de instância
+# @classmethod - cls, método de classe
+# @staticmethod - método estático (❌self, ❌cls)
+class Connection:
+    def __init__(self, host='localhost'):
