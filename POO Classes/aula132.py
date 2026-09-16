1b0cb54e3a70c15d6b5d5cb97163ac27caef0466
Manoel Victor Silveira <mano686@gmail.com>
2026-09-16 11:07:00 -0300

    Add Caneta class with constructor for color and cap attributes

---
+++ POO Classes/aula132.py
@@ -0,0 +1,13 @@
+# @property + @setter - getter e setter no modo Pythônico
+# - como getter
+# - p/ evitar quebrar código cliente
+# - p/ habilitar setter
+# - p/ executar ações ao obter um atributo
+# Atributos que começar com um ou dois underlines
+# não devem ser usados fora da classe.
+#  🐍🤓🤯🤯🤯🤯
+class Caneta:
+    def __init__(self, cor):
+        # private protected
+        self.cor = cor
+        self._cor_tampa = None
