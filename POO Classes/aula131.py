4622bdb14d1ae5505b3f1ff625ad1aab715e3811
Manoel Victor Silveira <mano686@gmail.com>
2026-09-16 11:05:54 -0300

    Refactor Caneta class to include getter methods and property for color attribute

--- POO Classes/aula131.py
+++ POO Classes/aula131.py
@@ -15,3 +15,15 @@
 class Caneta:
     def __init__(self, cor):
         self.cor_tinta = cor
+
+    def get_cor(self):
+        return self.cor_tinta
+
+    @property
+    def cor(self):
+        return self.cor_tinta
+
+caneta = Caneta('Azul')
+print(caneta.cor)
+# print(caneta.cor_tinta)  
+print(caneta.get_cor())  
