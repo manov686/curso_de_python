0d0f9c49c179e860c50207b6aeb86c41a3c4eb08
Manoel Victor Silveira <mano686@gmail.com>
2026-09-16 12:40:31 -0300

    Refactor Caneta class to improve property methods and add cor_tampa attribute

--- POO Classes/aula132.py
+++ POO Classes/aula132.py
@@ -10,17 +10,29 @@ class Caneta:
     def __init__(self, cor):
         # private protected
         self._cor = cor
+        self._cor_tampa = None
 
     @property
     def cor(self):
-        print('PROPERTY')
+        # print('PROPERTY')
         return self._cor
 
     @cor.setter
     def cor(self, valor):
-        print('SETTER', valor)
+        # print('SETTER', valor)
         self._cor = valor
+
+    @property
+    def cor_tampa(self):
+        return self._cor_tampa
+
+    @cor_tampa.setter
+    def cor_tampa(self, valor):
+        self._cor_tampa = valor
+
 
 caneta = Caneta('Azul')
 caneta.cor = 'Vermelho'
+caneta.cor_tampa = 'Preta'
 print(caneta.cor)
+print(caneta.cor_tampa)
