6168c91748a051eff27cab86014e10000f0d30fa
Manoel Victor Silveira <mano686@gmail.com>
2026-09-18 09:22:26 -0300

    Refactor Foo class to correct private attribute naming and update public method for better encapsulation demonstration

--- POO Classes/aula133.py
+++ POO Classes/aula133.py
@@ -11,6 +11,7 @@
 #       _NomeClasse__nome_attr_ou_method
 #       só DEVE ser usado na classe em que foi
 #       declarado.
+
 from functools import partial
 
 
@@ -18,21 +19,56 @@ class Foo:
     def __init__(self):
         self.public = 'isso é público'
         self._protected = 'isso é protegido'
-        self.__private = 'isso é privado'
+        self.__exemplo = 'isso é private'
 
-        self._method_protected()
+    def metodo_publico(self):
+        # self._metodo_protected()
+        # print(self._protected)
+        print(self.__exemplo)
+        self.__metodo_private()
+        return 'metodo_publico'
 
-    def public_method(self):
-        # return 'isso é um método público'
-        print(self.__private)
-        return '__private'
-    
-    def _method_protected(self):
-        print('isso é um método protegido')
-        return 'isso é protegido'
+    def _metodo_protected(self):
+        print('_metodo_protected')
+        return '_metodo_protected'
+
+    def __metodo_private(self):
+        print('__metodo_private')
+        return '__metodo_private'
+
 
 f = Foo()
-
-print(f.public_method())
 # print(f.public)
+print(f.metodo_publico())
+
+
+# from functools import partial
+
+
+# class Foo:
+#     def __init__(self):
+#         self.public = 'isso é público'
+#         self._protected = 'isso é protegido'
+#         self.__private = 'isso é privado'
+
+#         self._method_protected()
+
+#     def public_method(self):
+#         # return 'isso é um método público'
+#         print(self.__private)
+#         return '__private'
+    
+#     def _method_protected(self):
+#         print('isso é um método protegido')
+#         return 'isso é protegido'
+        
+#     def __method_private(self):
+#         print('priv method')
+#         return 'private method'
+
+
+# f = Foo()
+# print(f._Foo__method_private())
 # print(f.public_method())
+# # print(f.public)
+# # print(f.public_method())
