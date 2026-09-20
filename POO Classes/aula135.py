57f0a97ca7b2d21e694e5d704b8616378b199f23
Manoel Victor Silveira <mano686@gmail.com>
2026-09-20 09:55:39 -0300

    Add aula135.py to implement Carrinho class for managing product aggregation

---
+++ POO Classes/aula135.py
@@ -0,0 +1,16 @@
+# Relações entre classes: associação, agregação e composição
+# Agregação é uma forma mais especializada de associação
+# entre dois ou mais objetos. Cada objeto terá
+# seu ciclo de vida independente.
+# Geralmente é uma relação de um para muitos, onde um
+# objeto tem um ou muitos objetos.
+# Os objetos podem viver separadamente, mas pode
+# se tratar de uma relação onde um objeto precisa de
+# outro para fazer determinada tarefa.
+# (existem controvérsias sobre as definições de agregação).
+class Carrinho:
+    def __init__(self):
+        self.produtos = []
+
+    def total(self):
+        return sum([p.preco for p in self.produtos])
