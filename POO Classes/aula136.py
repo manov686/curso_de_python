5c46996c02f664262e34d2bfce7721a51e8e0465
Manoel Victor Silveira <mano686@gmail.com>
2026-09-21 16:58:00 -0300

    Add Client and Address classes with address management functionality

--- POO Classes/aula136.py
+++ POO Classes/aula136.py
@@ -3,3 +3,16 @@
 # Mas nela, quando o objeto "pai" for apagado, todas
 # as referências dos objetos filhos também são
 # apagadas.
+
+class Client:
+    def __init__(self, name):
+        self.name = name
+        self.adresses = []
+
+    def add_address(self, street, number):
+        self.adresses.append()
+
+class Address:
+    def __init__(self, street, number):
+        self.street = street
+        self.number = number
