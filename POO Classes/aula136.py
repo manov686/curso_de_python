ae625139d6b5db90db2ca5115cea8970d456d3aa
Manoel Victor Silveira <mano686@gmail.com>
2026-09-21 16:55:06 -0300

    Add aula136.py to explain relationships between classes: association, aggregation, and composition

---
+++ POO Classes/aula136.py
@@ -0,0 +1,5 @@
+# Relações entre classes: associação, agregação e composição
+# Composição é uma especialização da agregação.
+# Mas nela, quando o objeto "pai" for apagado, todas
+# as referências dos objetos filhos também são
+# apagadas.
