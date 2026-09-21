6f1a7cf30f9308042ba6ea3ab96f41f7666ce868
Manoel Victor Silveira <mano686@gmail.com>
2026-09-21 16:54:43 -0300

    Implement Chart class for product management with methods for total price calculation and listing products

--- POO Classes/aula135.py
+++ POO Classes/aula135.py
@@ -8,9 +8,37 @@
 # se tratar de uma relação onde um objeto precisa de
 # outro para fazer determinada tarefa.
 # (existem controvérsias sobre as definições de agregação).
-class Carrinho:
-    def __init__(self):
-        self.produtos = []
 
-    def total(self):
-        return sum([p.preco for p in self.produtos])
+class Chart:
+
+    def __init__(self):
+        self._products = []
+
+    def ttlprice(self):
+        return sum(p.price for p in self._products)
+
+    def insert_product(self, *products):
+        for p in products:
+            self._products.append(p)
+
+    def list_products(self):
+        for p in self._products:
+            print(p.name, f'R$ {p.price:.2f}')
+
+
+class Product:
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+chart = Chart()
+
+p1, p2 = Product('camiseta', 50), Product('bermuda', 100)
+
+chart.insert_product(p1, p2)
+
+chart.list_products()
+print()
+print(f'Total: R$ {chart.ttlprice():.2f}')
