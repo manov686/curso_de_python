3a567fff8901de7468b37f8eeacaea9ddb87fd05
Manoel Victor Silveira <mano686@gmail.com>
2026-09-21 17:05:10 -0300

    Refactor Client class to improve address listing functionality and enhance Address class with deletion message

--- POO Classes/aula136.py
+++ POO Classes/aula136.py
@@ -12,7 +12,29 @@ class Client:
     def add_address(self, street, number):
         self.adresses.append(Address(street, number))
 
+    def list_adresses(self):
+        for address in self.adresses:
+            print(address.street, address.number)
+
 class Address:
     def __init__(self, street, number):
         self.street = street
         self.number = number
+
+    def __del__(self):
+        print(f'Address {self.street}, {self.number} deleted.')
+
+
+
+client1 = Client('Luiz')
+client1.add_address('Rua A', 123)
+
+client2 = Client('Arthur')
+client2.add_address('Rua B', 456)
+
+# print(client1.adresses[0].street)
+# print(client2.adresses[0].street)
+# print()
+client1.list_adresses()
+client2.list_adresses()
+print('###### Here the code finishes. #####')
