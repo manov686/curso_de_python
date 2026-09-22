55cbf6be7a5e2e243584963d149a8bb39260f74f
Manoel Victor Silveira <mano686@gmail.com>
2026-09-22 16:57:05 -0300

    Implement Person class with name and surname attributes and method to display class name

--- POO Classes/aula138.py
+++ POO Classes/aula138.py
@@ -1,6 +1,22 @@
 ## Herança simples
 
-class Foo:
+class Person:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+    def tell_class_name(self):
+        print(self.name, self.surname, self.__class__.__name__)
+
+class Client(Person):
     ...
 
-help(Foo)
+class Student(Person):
+    ...
+
+
+c1 = Client('Manoel','Silveira')
+a1 = Student('José','Otávio')
+
+c1.tell_class_name()
+a1.tell_class_name()
