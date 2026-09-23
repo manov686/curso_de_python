528deb1c9372f0bf7fee383e82d2cc088a7aa526
Manoel Victor Silveira <mano686@gmail.com>
2026-09-23 18:27:00 -0300

    Uncomment MinhaString class and its usage in aula139.py for demonstration of super() and method overriding

--- POO Classes/aula139.py
+++ POO Classes/aula139.py
@@ -3,13 +3,28 @@
 #   -> super class, base class, parent class
 # Classes filhas (Cliente)
 #   -> sub class, child class, derived class
-class MinhaString(str):
-    def upper(self):
-        print('CHAMOU UPPER')
-        retorno = super(MinhaString, self).upper()
-        print('DEPOIS DO UPPER')
-        return retorno
+# class MinhaString(str):
+#     def upper(self):
+#         print('CHAMOU UPPER')
+#         retorno = super(MinhaString, self).upper()
+#         print('DEPOIS DO UPPER')
+#         return retorno
 
 
-string = MinhaString('Luiz')
-print(string.upper())
+# string = MinhaString('Luiz')
+# print(string.upper())
+
+class A:
+    atributo_a = 'valor'
+    def metodo(self):
+        print('A')
+
+class B(A):
+    atributo_b = 'valor'
+    def metodo(self):
+        print('B')
+
+class C(B):
+    atributo_c = 'valor'
+    def metodo(self):
+        print('C')
