5ead19c63df17d2baebdddf5b464f6d4c4dcee23
Manoel Victor Silveira <mano686@gmail.com>
2026-09-23 18:29:23 -0300

    Refactor aula139.py to enhance class attribute definitions and implement method chaining in class C

--- POO Classes/aula139.py
+++ POO Classes/aula139.py
@@ -15,16 +15,23 @@
 # print(string.upper())
 
 class A:
-    atributo_a = 'valor'
+    atributo_a = 'valor a'
     def metodo(self):
         print('A')
 
 class B(A):
-    atributo_b = 'valor'
+    atributo_b = 'valor b'
     def metodo(self):
         print('B')
 
 class C(B):
-    atributo_c = 'valor'
+    atributo_c = 'valor c'
     def metodo(self):
+        super().metodo()
         print('C')
+
+c = C()
+# print(c.atributo_a)
+# print(c.atributo_b)
+# print(c.atributo_c)
+c.metodo()
